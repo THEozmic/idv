2f35137f2999cf6bcf6f6cd4a678766685a953bb
THEozmic <michaelozoemena2@gmail.com>
2016-12-29 20:58:25 +0100

    This is the genesis

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IDEYVEX | Rant anonymously</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="left-header">
+			<div class="ad">
+				<div class="ad-big-text">Hello there, I'm an ad.</div>
+				<div class="ad-small-text">I love looking sleek.</div>
+			</div>
+		</div>
+		<div class="page">
+			<div class="page-section">
+				<div class="page-header">
+					<div class="brand">I DEYVEX</div>
+					<div class="other-links">
+					 	<div class="link-container"><a class= "link primary" href="post/">I dey vex ...</a></div>
+						<div class="link-container"><a class= "link" href="about/">About</a></div>
+						<div class="link-container"><a class= "link" href="trending/">Trending</a></div>
+						<div class="link-container"><a class= "link" href="contact/">Contact</a></div>
+					</div>
+				</div>
+				<div class="page-content">
+					 <div class="nav"><a class="link" href="prev">← Back</a></div>
+					 <div class="vexation">
+					 	<div class="vexation-content">
+					 		<a href="vexation-555">I dey vex cos the dude I dated for 9years suddenly left without looking back. It sucks to remain faithful!</a>
+					 	</div>
+					 	<div class="vexation-footer">
+					 		<a class="actionable" href="#novex"><span class="novex emotion">novex 6</span></a>
+					 		<a class="actionable" href="#cheya"><span class="cheya emotion">cheya 1</span></a>
+					 		<a class="actionable" href="#abegi"><span class="abegi emotion">abegi 7</span></a>
+					 		<a class="actionable" href="#comment"><span class="comment emotion">comment 33</span></a>
+					 		<span class="inside"><a class="link" href="vexation-555">12.11.16 02:15pm →</a></span>
+					 	</div>
+					 </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
